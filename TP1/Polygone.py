08f73abdfb3671c01abf84297b847911800da608
Natsuyuki2 <64355639+Natsuyuki2@users.noreply.github.com>
2022-09-17 14:08:32 +0200

    Update from Papa - Working

--- TP1/Polygone.py
+++ TP1/Polygone.py
@@ -2,9 +2,11 @@ import turtle as t
 t.up()
 t.goto(0,-200)
 t.down()
-N=input("entrer une valeur:")
+N=input("Entrer une valeur:")
 N=int(N)
 for i in range(N):
     t.forward(100)
     t.left(360/N)
     t.done
+
+print("Program Terminated")
